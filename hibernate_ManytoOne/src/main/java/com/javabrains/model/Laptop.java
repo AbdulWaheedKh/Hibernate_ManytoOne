7c6d1b3e0d057aa58f99da64cc7dcc252aeabad8
waheed khan <waheedkhan6670@gmail.com>
2019-10-26 19:58:12 +0500

    Many to one mapping

---
+++ hibernate_ManytoOne/src/main/java/com/javabrains/model/Laptop.java
@@ -0,0 +1,35 @@
+package com.javabrains.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Laptop {
+
+	@Id
+	private int lid;
+	private String lname;
+	
+	@ManyToOne
+	private Student std;
+	
+	public int getLid() {
+		return lid;
+	}
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	@Override
+	public String toString() {
+		return "Laptop [lid=" + lid + ", lname=" + lname + "]";
+	}
+	
+	
+}
